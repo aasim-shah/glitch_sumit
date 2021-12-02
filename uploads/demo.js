24454c2973232aa918194e7ee5a4f77abacea8d4
Glitch (sumit-blog) <none>
2021-12-02 18:37:17 +0000

    💍🏯 Checkpoint ./uploads/demo.js:39277590/627 ./views/contacts.ejs:39277590/1842

--- uploads/demo.js
+++ uploads/demo.js
@@ -1,13 +1,10 @@
 
+
 const butReq = document.getElementById('butRequest');
 butReq.addEventListener('click', getContacts);
 
-const cbMultiple = document.getElementById('multiple');
 const cbName = document.getElementById('name');
-const cbEmail = document.getElementById('email');
 const cbTel = document.getElementById('tel');
-const cbAddress = document.getElementById('address');
-const cbIcon = document.getElementById('icon');
 const ulResults = document.getElementById('results');
 const preResults = document.getElementById('rawResults');
 
@@ -25,32 +22,18 @@ async function checkProperties() {
   if (supportedProperties.includes('name')) {
     enableProp(cbName);
   }
-  if (supportedProperties.includes('email')) {
-    enableProp(cbEmail);
-  }
   if (supportedProperties.includes('tel')) {
     enableProp(cbTel);
-  }
-  if (supportedProperties.includes('address')) {
-    enableProp(cbAddress);
-  }
-  if (supportedProperties.includes('icon')) {
-    enableProp(cbIcon);
   }
 }
 
 async function getContacts() {
   const props = [];
   if (cbName.checked) props.push('name');
-  if (cbEmail.checked) props.push('email');
   if (cbTel.checked) props.push('tel');
-  if (cbAddress.checked) props.push('address');
-  if (cbIcon.checked) props.push('icon');
-  
-  const opts = {multiple: cbMultiple.checked};
-  
+    
   try {
-    const contacts = await navigator.contacts.select(props, opts);
+    const contacts = await navigator.contacts.select(props);
     handleResults(contacts);
   } catch (ex) {
     ulResults.classList.toggle('error', true);
