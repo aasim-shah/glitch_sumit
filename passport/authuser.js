122729c0c27bb3f3622c4d37e6c253ed7970d562
Glitch (sumit-blog) <none>
2021-12-07 17:17:56 +0000

    ⛪️⏳ Checkpoint ./passport/authuser.js:39277590/182

--- passport/authuser.js
+++ passport/authuser.js
@@ -7,13 +7,13 @@ const User = require('../models/userModel')
 
 const tokenauth = async  (req , res , next) =>{
     try {
-      token = req.cookies.Token;
+     let  token = req.cookies.Token;
       let verifyUser = await jwt.verify(token , 'stgfingsecretkey')
         const authUser =await User.findOne({_id : verifyUser._id})
         req.user = authUser;
       next();
     } catch (error) {
-      return res.send('no auth');
+      return res.redirect('/user/login');
     console.log(error);
     console.log('hahha');
     }
